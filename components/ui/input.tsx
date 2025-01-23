7b9b69ec4deac9097792f3220087a39c364131cb
umutcandev <96707573+umutcandev@users.noreply.github.com>
2025-01-23 03:44:20 +0300

    fix: Remove unused helperText prop from Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,14 +2,12 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-    error?: boolean
-    helperText?: string
-  }
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, error, helperText, ...props }, ref) => {
+  ({ className, type, error, ...props }, ref) => {
     return (
       <input
         type={type}
